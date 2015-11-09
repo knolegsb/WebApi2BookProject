42760bfbf3881b79685d69df83b74e14da507abe
knolegsb <seon2you@hotmail.com>
2015-11-09 14:44:17 -0800

    added data query in db project

---
+++ WebApi2BookDb/Scripts/TestData.sql
@@ -0,0 +1,29 @@
+﻿declare @statusId int,
+	@taskId int,
+	@userId int
+
+if not exists (select * from [User] where UserName = 'bhogg')
+	INSERT into [dbo].[User] ([FirstName], [LastName], [UserName])
+		VALUES (N'Boss', N'Hoog', N'bhogg')
+
+if not exists (select * from [User] where UserName = 'jbob')
+	INSERT into [dbo].[User] ([FirstName], [LastName], [UserName])
+		VALUES (N'Jim', N'Bob', N'jbob')
+
+if not exists (select * from [User] where UserName = 'jdoe')
+	INSERT into [dbo].[User] ([FirstName], [LastName], [UserName])
+		VALUES (N'John', N'Doe', N'jdoe')
+
+if not exists (select * from dbo.Task where Subject = 'Test Task')
+begin
+	select top 1 @statusId = StatusId from Status order by StatusId;
+	select top 1 @userId = UserId from [User] order by UserId;
+
+	insert into dbo.Task(Subject, StartDate, StatusId, CreatedDate, CreatedUserId)
+		values('Test Task', getdate(), @statusId, getdate(), @userId);
+
+	set @taskId = SCOPE_IDENTITY();
+
+	INSERT [dbo].[TaskUser] ([TaskId], [UserId])
+		VALUES (@taskId, @userId)
+end
---
+++ WebApi2BookDb/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User](
+	[UserId]    BIGINT   IDENTITY (1, 1) NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOt NULL,
+	[UserName] NVARCHAR(50) NOT NULL,
+	[ts] [rowversion] NOT NULL,
+	CONSTRAINT [PK_User] PRIMARY KEY ([UserId])
+);
